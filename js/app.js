2a618cfe0692e06c41f3e2f69709eabbb8281b63
charlestechnologie <charleskurbitsh657@gmail.com>
2023-04-18 15:45:00 -0400

    first

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const loader = document.querySelector('.loader');
+
+window.addEventListener('load', () => {
+
+    loader.classList.add('fondu-out');
+
+
+})
+// const navbar = document.querySelector('nav');
+
+// window.addEventListener('load', () => {
+
+//     nav.classList.add('body');
+
+// })
